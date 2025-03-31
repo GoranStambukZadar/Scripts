bc627b87db8b83310cee791c8c76255f09f27343
GoranStambukZadar <znastidobrostoje@gmail.com>
2025-03-31 05:11:20 -0700

    Add files via upload

---
+++ GPerf/Bin/GPerf.cmd
@@ -0,0 +1,5 @@
+:: Install RamCleaner
+mkdir %windir%\Setup\Scripts
+copy /y emptystandbylist.exe %windir%\Setup\Scripts\emptystandbylist.exe
+copy /y RamCleaner.bat %windir%\Setup\Scripts\RamCleaner.bat
+schtasks /create /tn "RamCleaner" /xml "RamCleaner.xml" /ru "SYSTEM"
