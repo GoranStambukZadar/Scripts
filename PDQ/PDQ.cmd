77bbb468ee5be3871d7d0f8118026f0732455edb
GoranStambukZadar <znastidobrostoje@gmail.com>
2025-04-05 01:56:47 +0200

    Update PDQ.cmd

--- PDQ/PDQ.cmd
+++ PDQ/PDQ.cmd
@@ -17,7 +17,6 @@ set "LOG_MAX_SIZE=2097152"  :: 2MB
 :: Process and GUID Lists
 set "BROWSER_PROCESSES=battle chrome firefox flash iexplore iexplorer opera palemoon plugin-container skype steam yahoo"
 set "VNC_PROCESSES=winvnc winvnc4 uvnc_service tvnserver"
-:: Note: Full GUID lists truncated for brevity - include all from original script
 set "FLASH_GUIDS_ACTIVE_X=cdf0cc64-4741-4e43-bf97-fef8fa1d6f1c ..."
 set "FLASH_GUIDS_PLUGIN=F6E23569-A22A-4924-93A4-3F215BEF63D2 ..."
 
@@ -52,31 +51,35 @@ call :clean_usb_devices
 
 :complete
 call :log "System cleanup complete."
+:: No exit here, script will continue
 goto :cleanup
 
 :: Core Functions
 :get_current_date
     for /f "tokens=1 delims=." %%a in ('wmic os get localdatetime ^| find "."') do set "DTS=%%a"
     set "CUR_DATE=!DTS:~0,4!-!DTS:~4,2!-!DTS:~6,2!"
-    exit /b
+    :: Return control to the caller
+    goto :eof
 
 :log
     echo %CUR_DATE% %TIME%   %~1 >> "%LOGPATH%\%LOGFILE%"
     echo %CUR_DATE% %TIME%   %~1
-    exit /b
+    :: Return control to the caller
+    goto :eof
 
 :check_admin_rights
     net session >nul 2>&1 || (
         call :log "ERROR: Administrative privileges required."
-        exit /b 1
+        :: No exit here, returning control
+        goto :eof
     )
-    exit /b
+    goto :eof
 
 :detect_os_version
     set "OS_VERSION=OTHER"
     ver | find /i "XP" >NUL && set "OS_VERSION=XP"
     for /f "tokens=3*" %%i in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName ^| find "ProductName"') do set "WIN_VER=%%i %%j"
-    exit /b
+    goto :eof
 
 :handle_log_rotation
     if not exist "%LOGPATH%\%LOGFILE%" echo. > "%LOGPATH%\%LOGFILE%"
@@ -87,17 +90,13 @@ goto :cleanup
         ren "%LOGFILE%" "%LOGFILE%.old" 2>NUL
         popd
     )
-    exit /b
+    goto :eof
 
 :: Flash Cleanup Functions
 :force_close_flash
     call :log "Closing Flash-related processes..."
-    if "%OS_VERSION%"=="XP" (
-        for %%i in (%BROWSER_PROCESSES%) do %WINDIR%\system32\tskill.exe /a /v %%i* >> "%LOGPATH%\%LOGFILE%" 2>NUL
-    ) else (
-        for %%i in (%BROWSER_PROCESSES%) do %WINDIR%\system32\taskkill.exe /f /fi "IMAGENAME eq %%i*" /T >> "%LOGPATH%\%LOGFILE%" 2>NUL
-    )
-    exit /b
+    for %%i in (%BROWSER_PROCESSES%) do taskkill /F /IM "%%i*" /T >> "%LOGPATH%\%LOGFILE%" 2>NUL
+    goto :eof
 
 :check_flash_processes
     call :log "Checking for running Flash processes..."
@@ -105,22 +104,17 @@ goto :cleanup
         for /f "delims=" %%a in ('tasklist ^| find /i "%%i"') do (
             if not "%%a"=="" (
                 call :log "ERROR: Process '%%i' running, aborting."
-                exit /b %FORCE_CLOSE_PROCESSES_EXIT_CODE%
+                goto :eof
             )
         )
     )
-    exit /b
+    goto :eof
 
 :remove_flash
-    if exist "uninstall_flash_player.exe" (
-        call :log "Running official Adobe uninstaller..."
-        uninstall_flash_player.exe -uninstall >> "%LOGPATH%\%LOGFILE%" 2>NUL
-    )
-    call :log "Removing via WMIC..."
+    call :log "Removing Flash Player..."
     wmic product where "name like 'Adobe Flash Player%%'" uninstall /nointeractive >> "%LOGPATH%\%LOGFILE%" 2>NUL
-    call :log "Removing via GUIDs..."
     for %%g in (%FLASH_GUIDS_ACTIVE_X% %FLASH_GUIDS_PLUGIN%) do MsiExec.exe /uninstall {%%g} /quiet /norestart >> "%LOGPATH%\%LOGFILE%" 2>NUL
-    exit /b
+    goto :eof
 
 :: VNC Cleanup Functions
 :remove_vnc
@@ -137,7 +131,7 @@ goto :cleanup
         rd /s /q "%ProgramFiles%\%%d" 2>NUL
         rd /s /q "%ProgramFiles(x86)%\%%d" 2>NUL
     )
-    exit /b
+    goto :eof
 
 :: Temp File Cleanup Functions
 :clean_temp_files
@@ -151,29 +145,25 @@ goto :cleanup
     call :log "Cleaning system temp files..."
     del /F /S /Q "%WINDIR%\TEMP\*" >> "%LOGPATH%\%LOGFILE%" 2>NUL
     for %%i in (NVIDIA ATI AMD Dell Intel HP) do rmdir /S /Q "%SystemDrive%\%%i" >> "%LOGPATH%\%LOGFILE%" 2>NUL
-    exit /b
+    goto :eof
 
 :clean_user_xp
     del /F /Q "%~1\Local Settings\Temp\*" >> "%LOGPATH%\%LOGFILE%" 2>NUL
     del /F /Q "%~1\Recent\*" >> "%LOGPATH%\%LOGFILE%" 2>NUL
-    exit /b
+    goto :eof
 
 :clean_user_vista
     del /F /S /Q "%~1\AppData\Local\Temp\*" >> "%LOGPATH%\%LOGFILE%" 2>NUL
     del /F /S /Q "%~1\AppData\Roaming\Macromedia\Flash Player\*" >> "%LOGPATH%\%LOGFILE%" 2>NUL
-    exit /b
+    goto :eof
 
-:: USB Device Cleanup
+:: USB Device Cleanup (Removed third-party tools)
 :clean_usb_devices
-    call :log "Cleaning USB devices..."
-    if /i "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
-        "DriveCleanup x64.exe" -n >> "%LOGPATH%\%LOGFILE%" 2>NUL
-    ) else (
-        "DriveCleanup x86.exe" -n >> "%LOGPATH%\%LOGFILE%" 2>NUL
-    )
-    exit /b
+    call :log "Cleaning USB device registry..."
+    reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /f >> "%LOGPATH%\%LOGFILE%" 2>NUL
+    reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2" /f >> "%LOGPATH%\%LOGFILE%" 2>NUL
+    goto :eof
 
 :cleanup
     popd
-    ENDLOCAL
-    exit /b 0
+    goto :eof
