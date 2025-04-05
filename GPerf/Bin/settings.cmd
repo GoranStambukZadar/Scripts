b102f2e4cc6c3592793c3a9dabb7b289a8e69a24
GoranStambukZadar <znastidobrostoje@gmail.com>
2025-04-05 03:37:41 +0200

    Delete GPerf directory

--- GPerf/Bin/settings.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-:: Install RamCleaner
-mkdir %windir%\Setup\Scripts
-copy /y emptystandbylist.exe %windir%\Setup\Scripts\emptystandbylist.exe
-copy /y RamCleaner.bat %windir%\Setup\Scripts\RamCleaner.bat
-schtasks /create /tn "RamCleaner" /xml "RamCleaner.xml" /ru "SYSTEM"
-
-
