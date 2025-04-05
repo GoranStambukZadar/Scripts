f4236c56d49204c76fdf28555f731a8cc66d2d20
GoranStambukZadar <znastidobrostoje@gmail.com>
2025-04-05 03:19:50 +0200

    Add files via upload

--- GPerf/Bin/settings.cmd
+++ GPerf/Bin/settings.cmd
@@ -1,11 +1,5 @@
 @echo off
 
-:: Combine services
-set servicesPath=HKLM\SYSTEM\CurrentControlSet\Services
-for /f "tokens=*" %%i in ('reg query "%servicesPath%" /s /f "" ^| findstr "HKEY_LOCAL_MACHINE"') do (
-    reg add "%%i" /v SvcHostSplitDisable /t REG_DWORD /d 1 /f
-)
-
 :: Install RamCleaner
 mkdir %windir%\Setup\Scripts
 copy /y emptystandbylist.exe %windir%\Setup\Scripts\emptystandbylist.exe
