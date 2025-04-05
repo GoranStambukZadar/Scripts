01ccc989cfd197365d6bc79032f8ee5d9d09fd07
GoranStambukZadar <znastidobrostoje@gmail.com>
2025-04-05 03:23:48 +0200

    Add files via upload

--- GPerf/Setup.cmd
+++ GPerf/Setup.cmd
@@ -1,5 +1,5 @@
 @echo off
-Title GShield && Color 0b
+Title GPerf && Color 0b
 
 :: Step 1: Elevate
 >nul 2>&1 fsutil dirty query %systemdrive% || echo CreateObject^("Shell.Application"^).ShellExecute "%~0", "ELEVATED", "", "runas", 1 > "%temp%\uac.vbs" && "%temp%\uac.vbs" && exit /b
